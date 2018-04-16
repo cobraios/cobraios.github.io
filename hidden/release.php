6221f26d8402c7f66719004d706e983231a87896
Cobra IOS <iphone.cobraios@gmail.com>
2018-04-16 08:49:22 -0500

    2

--- hidden/release.php
+++ hidden/release.php
@@ -2,5 +2,5 @@
         include_once 'checker.php';
         
         if (checkUDID($_SERVER['HTTP_X_UNIQUE_ID']) == 'YES')
-		header('Location: http://your_repo.com/folder/release_dir');
+		header('Location: https"//cobraios.github.io/hidden/release_dir');
 ?>
