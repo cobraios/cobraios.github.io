2b732cb05d9926b2c4a8649180153e70bb8944d7
Cobra IOS <iphone.cobraios@gmail.com>
2018-04-16 07:46:05 -0500

    1

--- hidden/checker.php
+++ hidden/checker.php
@@ -1,7 +1,7 @@
 <?php
 	function checkUDID($udid) 
 	{
-		$acceptedUDID = array('abcd', 'efgh');
+		$acceptedUDID = array('909a4ba097c70dd2067966aa4b3d0c219dfece62', 'efgh');
 		
 		if (in_array($udid, $acceptedUDID))
 			return 'YES';
